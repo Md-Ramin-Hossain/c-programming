5ed5acca3187f81b52cc010efc941bc24189ff57
Md Ramin Hossain <129541085+Md-Ramin-Hossain@users.noreply.github.com>
2023-04-01 17:10:15 +0600

    Add files via upload

---
+++ Setup/resolve.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+
+int n,a,b,c;
+printf("Here is two option\n");
+printf("1. open manu\n");
+printf("Please give me a code");
+scanf("%d",&n);
+
+
+
+if(n==1 || n==2)
+{
+
+    printf("please choice your option\n");
+    printf("What you want from here\n");
+    printf("1.brackfast\n");
+    printf("2.Launce\n");
+    printf("after launce\n");
+    printf("Diner");
+}
+
+else
+printf("please enter the right option");
+
+
+return 0;
+}
